b16c10f1597e4341d57362987a516dbd1fdf909f
Kris Chi <chilaoqi@gmail.com>
2022-06-21 00:40:47 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
   siteMetadata: {
     siteTitle: 'Kris Running Page',
     siteUrl: 'https://run.chixq.com',
-    logo: 'https://github.com/chixq/running_page/blob/a914ae46306d8cfc298361425c746495e99a5edd/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20220620211943.jpg?raw=true',
+    logo: 'https://s3.bmp.ovh/imgs/2022/06/21/2aa9cc93291ee92d.jpg',
     description: '飞致云南区跑步记录',
     navLinks: [
       {
@@ -65,7 +65,7 @@ module.exports = {
         background_color: '#e1e1e1',
         theme_color: '#e1e1e1',
         display: 'minimal-ui',
-        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon.ico', // This path is relative to the root of the site.
       },
     }
   ],
